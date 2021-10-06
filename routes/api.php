9a9facbc67cc9b838641d4f459e00c2ffe4857f3
wayosu <wahyusman10@gmail.com>
2021-10-07 02:58:46 +0800

    API Login

--- routes/api.php
+++ routes/api.php
@@ -23,4 +23,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::get('products', [ProductController::class, 'all']);
 Route::get('categories', [ProductCategoryController::class, 'all']);
-Route::get('users', [UserController::class, 'register']);
+Route::post('register', [UserController::class, 'register']);
+Route::post('login', [UserController::class, 'login']);
